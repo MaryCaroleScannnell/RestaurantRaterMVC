65ca3176a96634699249d7516be73f29147bacb0
MaryCaroleScannnell <73565044+MaryCaroleScannnell@users.noreply.github.com>
2021-03-27 09:15:07 -0400

    Create Functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -15,5 +15,23 @@ namespace RestaurantRater.Controllers
         {
             return View(_db.Restaurants.ToList());
         }
+        //Get (Create)
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Post (save the create)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        { 
+            if(ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
     }
 }
